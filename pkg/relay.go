ebbb38607ead23b61af28e16320b3f426c523c65
Materii <contact@chantzlarge.com>
2023-04-19 19:29:55 -0400

    Removed example code from relay

--- pkg/relay.go
+++ pkg/relay.go
@@ -1,24 +1,7 @@
 package nostr
 
-import (
-	"net/http"
-	"sync"
-
-	"golang.org/x/time/rate"
-)
-
 func NewRelay() *Relay {
 	return &Relay{}
-}
-
-// NOTE: copied from cmd/chat.go
-
-// Subscriber represents a subscriber.
-// Messages are sent on the msgs channel and if the client
-// cannot keep up with the messages, closeSlow is called.
-type Subscriber struct {
-	msgs      chan []byte
-	closeSlow func()
 }
 
 type Relay struct {
@@ -30,26 +13,4 @@ type Relay struct {
 	Software      []byte       `json:"software,omitempty"`
 	Version       []byte       `json:"version,omitempty"`
 	Limitations   *Limitations `json:"limitations,omitempty"`
-
-	// subscriberMessageBuffer controls the max number
-	// of messages that can be queued for a subscriber
-	// before it is kicked.
-	//
-	// Defaults to 16.
-	subscriberMessageBuffer int
-
-	// publishLimiter controls the rate limit applied to the publish endpoint.
-	//
-	// Defaults to one publish every 100ms with a burst of 8.
-	publishLimiter *rate.Limiter
-
-	// logf controls where logs are sent.
-	// Defaults to log.Printf.
-	logf func(f string, v ...interface{})
-
-	// serveMux routes the various endpoints to the appropriate handler.
-	serveMux http.ServeMux
-
-	subscribersMu sync.Mutex
-	subscribers   map[*Subscriber]struct{}
 }
